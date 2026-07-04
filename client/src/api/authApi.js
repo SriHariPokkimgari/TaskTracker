65d1d0fb64232d6153d206c84283a145ba0e6418
Sri Hari <pokkimgarisrihari5@gmail.com>
2026-07-04 16:47:41 +0530

    Add environment variables

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.PUBLIC_API_URL}/api/auth`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
